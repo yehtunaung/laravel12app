f645bc4a9332a5ae7961e66c4bfdf5c8a06bdaa0
Ye Htun Aung <yehtunaung@uab.com.mm>
2025-07-22 14:47:16 +0630

    update alpinejs for npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,33 @@
-import './bootstrap';
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
+import persist from '@alpinejs/persist';
+import 'flowbite';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+if (!window.Alpine) {
+    Alpine.plugin(focus);
+    Alpine.plugin(persist);
+    window.Alpine = Alpine;
+    Alpine.start();
+}
+
+
+function initializeFlowbite() {
+    initFlowbite();
+}
+
+document.addEventListener("livewire:navigating", () => {
+    initializeFlowbite();
+});
+
+document.addEventListener("livewire:navigated", () => {
+    initializeFlowbite();
+});
+document.addEventListener("livewire:load", () => {
+    initializeFlowbite();
+});
+
+// Initialize on page load
+initializeFlowbite();
+
+
